b0dd35c39227df5ef2e60ffc5098fa44d5bed325
talha <talha>
2020-03-12 17:39:11 +0300

    requiremets.txt file and virtual environment

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\zet\Documents\workspace\dev\cankaya\ceng-407-408-2019-2020-Rad.io---Online-Radio-Application\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
