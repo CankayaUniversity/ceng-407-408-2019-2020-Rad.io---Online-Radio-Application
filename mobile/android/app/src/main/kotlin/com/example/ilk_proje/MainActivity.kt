50d28a3983511e00fa78861969d5e313f94490c2
Berkay Karadayı <48567842+berkaykaradayi@users.noreply.github.com>
2020-04-22 18:28:28 +0300

    Add files via upload

---
+++ mobile/android/app/src/main/kotlin/com/example/ilk_proje/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.ilk_proje
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
